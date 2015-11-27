f5fe331499ad1de319943652e108126345d412a6
Nabil A <nabilabdu@gatech.edu>
2015-11-27 08:28:48 -0600

    completed tentative oc/ec corrections

--- data-raw/ocec_corrections.R
+++ data-raw/ocec_corrections.R
@@ -97,8 +97,10 @@ early_ocec %>%
 #   select(-MethodName) %>% group_by(StateCountySite, Date, Species, ParamCode) %>%
 #   mutate(num_vals = length(avg_conc)) %>% filter(ParamCode == 88329, num_vals > 1)
 
+# METHOD CODE goes away here, but it's needed for the
 early_ocec_nodups <- early_ocec %>%
-  group_by(StateCountySite, Date, Species, ParamCode, lat, long, Year) %>%
+  select(-MethodName) %>%
+  group_by(StateCountySite, Date, Species, ParamCode, lat, long, MethodCode, Year) %>%
   summarize(mean_conc = mean(avg_conc))
 
 # to see that this worked:
@@ -109,7 +111,7 @@ early_ocec_nodups <- early_ocec %>%
 
 ## so at this stage, for each: site, date, paramcode, there is only one value.
 
-
+# all OC concentrations, with no duplicate measurements
 oc_res <- early_ocec_nodups %>%
   filter(Species == "OC25") %>%
   group_by(Date, StateCountySite) %>%
@@ -121,8 +123,9 @@ oc_res <- early_ocec_nodups %>%
 # test that only one value per siteday:
 # oc_res %>% group_by(StateCountySite, Date) %>% mutate(len = length(mean_conc)) %>% filter(len > 1) # WORKS!
 
-
+# ~ 2 min
 ec_res <- early_ocec_nodups %>%
+  ungroup %>%
   filter(Species == "EC25") %>%
   group_by(Date, StateCountySite) %>%
   mutate(orig_num_rows = length(ParamCode)) %>%
@@ -130,24 +133,13 @@ ec_res <- early_ocec_nodups %>%
   ddply(.(Date, StateCountySite), ec_site_filt) %>%
   arrange(Date, StateCountySite, ParamCode) %>% tbl_df
 
-# NEW STUFF
+# check uniqueness:
+# aa <- ec_res %>% group_by(Date, StateCountySite, ParamCode) %>%
+#     mutate(numvals = length(ParamCode)) %>% filter(numvals > 1)
 
-# "ones" are: the concentrations for which the duplicates are not removed. so,
-# they are  temporarily (i.e., by a hack) removed
-ones <- ec_res %>% filter(ParamCode == 88307) %>%
-  group_by(Date, StateCountySite) %>% mutate(len = length(ParamCode)) %>%
-  filter(len > 1) %>% select(-MethodName)
-
-ec_no_dups <- ones %>%
-  group_by(StateCountySite, Date, Species, lat, long) %>%
-  summarize(ec_imp = mean(avg_conc)) %>%  # averages over POC
-  select(StateCountySite:Species, avg_conc = ec_imp, everything())
-
-# combine the (manually) dup-eliminated concentrations, with the
-# automatically dup-elim'ed concentrations
-ec_res2 <- ec_res %>%
-  anti_join(ec_no_dups) %>%
-  bind_rows(ec_no_dups)
+# note that of the four paramcodes present in ec_res, only one (88307) doesn't
+# always have unique values for each siteday. Now need a means of selecting
+# based on the methodcode. For now, will just take the minimum as a proxy
 
 
 ## compute corrections
@@ -157,14 +149,14 @@ ec_res2 <- ec_res %>%
 # for OC25, corrections only on 88305. Corresponding EC values would have
 # paramcode of 88307:
 # "problem EC" - a misnomer
-prob_ec <- ec_res2 %>% filter(ParamCode == 88307) %>% select(-num_rows)
+prob_ec <- ec_res %>% filter(ParamCode == 88307) %>% select(-orig_num_rows)
 
 # need corresponding EC measurements, for the OC corrections
-prob_oc_res <- oc_res %>% filter(ParamCode == 88305)
+prob_oc_res <- oc_res %>% filter(ParamCode == 88305) # OC measurements to be corrected
 prob_ocec <- prob_oc_res %>%
-  spread(Species, avg_conc) %>%
+  spread(Species, mean_conc) %>%
   left_join(
-    spread( filter(prob_ec, ParamCode == 88307),  Species, avg_conc),
+    spread( prob_ec, Species, mean_conc),
     by = c("Date", "StateCountySite", "lat", "long", "Year")
   ) %>%
   filter(!is.na(EC25))
@@ -172,58 +164,91 @@ prob_ocec <- prob_oc_res %>%
 # generate oc IMPROVE corrected concentrations
 oc_imp <- prob_ocec %>%
   rename(SamplerCode = MethodCode.x) %>%
-  gen_oc_imp %>%
+  gen_oc_imp %>% tbl_df %>%
   select(StateCountySite:Date, imp_conc = oc_imp, lat:long, Year)
 
 # combine all OC concentrations
-# COMPLETE CORRECTED EARLY OC DATA
-fin_oc_res <- oc_res %>% anti_join(prob_oc_res) %>%
-  select(StateCountySite:Date, imp_conc = avg_conc, lat:long, Year) %>%
-  bind_rows(oc_imp) %>%
+# NB: need to have completed the corrected OC data before this stage
+# Note: what to do about naming conc column for oc_imp? don't want to join with mean_conc
+# DESC: this works by taking the OC concentrations which haven't been corrected,
+#   and joining (i.e., row-binding) with those that have been
+fin_oc_res <- oc_res %>%
+  anti_join(prob_oc_res) %>%
+  select(StateCountySite:Date, imp_conc = mean_conc, lat:long, Year) %>%
+  bind_rows(oc_imp) %>% # this comes from prob_ocec from prob_oc_res from oc_res
   filter(!is.na(imp_conc)) %>%
   select(-Year) %>% mutate(Species = "OC25")
 
+# no missing values, and unique measurements per sitedate
 fin_oc_res %>% is.na %>% sum # ZERO!
+fin_oc_res %>% group_by(StateCountySite, Date, Species) %>%
+  mutate(len = length(imp_conc)) %>% filter(len > 1) %>% nrow
 
 # only ~500 rows with ParamCode 88321, so we just get rid of them
 
 # performs the corrections for EC, whether that's by addition or multiplication,
 # then combines both
 ec_res2 <- ec_res2 %>% filter(ParamCode != 88321)
+
+# EC corrections by adding
 adding_ec_res <- ec_res2 %>%
-  filter(ParamCode != 88307) %>% # code for not adding
+  filter(ParamCode != 88307) %>% # paramcode that is not corrected by adding
   group_by(Date, StateCountySite, long, lat, Year) %>%
   summarize(ec_imp = sum(avg_conc)) %>%
   select(StateCountySite, Date, ec_imp, lat, long)
 
+# no missing values, unique vals per sitedate
+adding_ec_res %>% is.na %>% sum
+adding_ec_res %>% group_by(StateCountySite, Date) %>%
+  mutate(len = length(ec_imp)) %>% filter(len > 1)
+
+
+# EC corrections by mutliplication factor
+# the paramcode has multiple POC measurements for each sitedate, so we first
+# average over them all to get unique vals by sitedate
 mult_ec_res <- ec_res2 %>%
   filter(ParamCode == 88307) %>%
   group_by(Date, StateCountySite, long, lat, Year) %>%
-  mutate(ec_imp = 1.3 * avg_conc) %>%
-  select(-avg_conc) %>% ungroup %>%
-  select(StateCountySite, Date, ec_imp, lat, long)
+  summarize(ec_conc = mean(avg_conc)) %>%
+  mutate(ec_imp = 1.3 * ec_conc) %>%
+  select(-ec_conc) %>% ungroup %>%
+  select(StateCountySite, Date, ec_imp, lat, long) %>%
+  arrange(StateCountySite, Date)
 
 fin_ec_res <- adding_ec_res %>%
   bind_rows(mult_ec_res) %>%
   mutate(Species = "EC25") %>%
-  rename(imp_conc = ec_imp)
+  rename(imp_conc = ec_imp) %>%
+  arrange(StateCountySite, Date)
 
+
+# need to work on this
 fin_ec_res %>% is.na %>% sum # ZERO!!!
+fin_ec_res %>% group_by(StateCountySite, Date, Species) %>%
+  mutate(len = length(imp_conc)) %>% filter(len > 1) %>% nrow
 
+# complete corrected OC/EC data for earlier years
+fin_early_ocec <- fin_ec_res %>% bind_rows(fin_oc_res)
 
-fin_ocec_res <- fin_ec_res %>% bind_rows(fin_oc_res)
 
 ####
 
 # AQS OC/EC
 
-corrected_aqs_ocec <- later_ocec %>%
-  select(StateCountySite:Date, imp_conc = avg_conc, Species) %>%
+# corrections for OC/EC performed for earlier years
+# here, later OC/EC concentrations combined with earlier concentrations
+corrected_aqs_ocec <- fin_later_ocec %>% ungroup %>%
+  select(StateCountySite:Date, imp_conc = mean_conc, Species) %>%
   bind_rows(
-    select(fin_ocec_res, -lat, -long)
+    select(fin_early_ocec, -lat, -long)
     ) %>%
-  rename(Conc_obs = imp_conc) %>% mutate(Year = year(ymd(Date)))
+  rename(Conc_obs = imp_conc) %>% mutate(Year = year(ymd(Date))) %>%
+  arrange(StateCountySite, Date, Species)
 
+# no missing values, and unique vals by sitedate
 corrected_aqs_ocec %>% is.na %>% sum # ZERO !!!
+corrected_aqs_ocec %>% group_by(StateCountySite, Date, Species) %>%
+  mutate(len = length(Conc_obs)) %>% filter(len > 1) %>% nrow
+
 corrected_aqs_ocec %>% saveRDS("../hsa_data/corrected_ocec_all.rds")
 
